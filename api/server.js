0922d4acee681e5ab1d25ffb5a752b0a62d34698
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-05-18 11:48:00 -0500

    server running

--- api/server.js
+++ api/server.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cort = require('cors');
+const cors = require('cors');
 
+const { URI } = process.env;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+})
+    .then(()=>console.log("Conectado a DB cloud"))
+    .catch(console.error);
+
+app.listen(3001, ()=>console.log("Server started on port 3001"))
